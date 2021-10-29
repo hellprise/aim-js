95d9c2948d65af1bd4fe9931fff1f7a475cca095
Your Name <you@example.com>
2021-10-29 23:55:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,100 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+const board = document.querySelector('#board')
+let time = 0
+let score = 0
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // таким образом мы обнуляем поведение кнопки(которая на самом деле является ссылкой) и решётка, которая прописана в атрибуте href не появляется в адресной строке.
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
+        startGame()
+    }
+})
+
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
+}
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame()
+    } else {
+        let current = --time
+        if (current < 10) {
+            current = `0${current}`
+        }
+        setTime(current)
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Счёт: <span class='primary'>${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    setColor(circle)
+
+    board.append(circle)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+}
+
+function getRandomColor() {
+    const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71', '#78DBE2', '#9966CC', '#003153']
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+
+function winTheGame() { // вызывать скрипт winTheGame() в консоли приложения, когда уже запустили игру.
+    function kill() {
+        const circle = document.querySelector('.circle')
+
+        if (circle) {
+            circle.click()
+        }
+    }
+    setInterval(kill, 42)
+}
